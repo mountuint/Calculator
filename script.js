ea3ff0f44d33d7fe77e909c74dd4d9a305fdfdbf
mountuint <mountuint@protonmail.com>
2026-07-06 12:44:48 +0300

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function addNumbers(firstNumber, secondNumber) {
+  return console.log(firstNumber + secondNumber);
+}
+
+function extractNumbers(firstNumber, secondNumber) {
+  return console.log(firstNumber - secondNumber);
+}
+
+function multiplyNumbers(firstNumber, secondNumber) {
+  return console.log(firstNumber * secondNumber);
+}
+
+function divideNumbers(firstNumber, secondNumber) {
+  return console.log(firstNumber / secondNumber);
+}
+
