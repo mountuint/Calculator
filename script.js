4b0ae4cb2c7e22416d12e741183cd8f49d73a2dd
mountuint <mountuint@protonmail.com>
2026-07-06 12:03:54 +0300

    remove unnecessary let declarations

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ for (let i = 0; i < btn.length; i++) {
   if (Number.isFinite(Number(btn[i].textContent))) {
     btn[i].addEventListener("click", () => {
       displayBox.innerText += btn[i].innerText;
-      console.log(displayBox.innerText);
+      //console.log(displayBox.innerText);
     });
   } else if (
     btn[i].textContent === "+" ||
@@ -46,28 +46,43 @@ for (let i = 0; i < btn.length; i++) {
     btn[i].textContent === "/"
   ) {
     btn[i].addEventListener("click", () => {
-      firstNumber = Number(displayBox.innerText);
+      firstNumber = displayBox.innerText;
+      console.log(firstNumber);
       displayBox.innerText += btn[i].innerText;
       operator = btn[i].innerText;
-      console.log(displayBox.innerText);
+      //console.log(displayBox.innerText);
       let operatorArray = displayBox.innerText.match(/[+\-*/]/g);
+      //console.log(operatorArray)
       if (operatorArray.length >= 2) {
         //console.log("it worked");
         //alert(displayBox.innerText)
-        let indexOfOperator = displayBox.innerText.indexOf(operator);
-        let firstNumber = Number(
+        let indexOfOperator = 0;
+        for (let i = 0; i < operatorArray.length; i++) {
+          console.log(displayBox.innerText);
+          console.log(operatorArray[i]);
+          indexOfOperator = displayBox.innerText.indexOf(operatorArray[i]);
+          console.log(indexOfOperator);
+        }
+        //console.log(indexOfOperator)
+        /*let firstNumber = Number(
           displayBox.innerText.substring(0, indexOfOperator),
-        );
+        );*/
+        firstNumber = displayBox.innerText.substring(0, indexOfOperator);
         alert(firstNumber);
         //alert(indexOfOperator)
         //alert(displayBox.innerText.length)
-        let secondNumber = Number(
+        secondNumber = displayBox.innerText.substring(
+          indexOfOperator + 1,
+          displayBox.innerText.length - 1,
+        );
+        /*let secondNumber = Number(
           displayBox.innerText.substring(
             indexOfOperator + 1,
             displayBox.innerText.length - 1,
           ),
-        );
-        alert(displayBox.innerText);
+        );*/
+        alert(secondNumber);
+        //alert(displayBox.innerText);
         //displayBox.innerText = operate(operator, firstNumber, secondNumber);
         //let displayBoxValue = displayBox.innerText
         let displayBoxValue = operate(operator, firstNumber, secondNumber);
@@ -90,6 +105,8 @@ for (let i = 0; i < btn.length; i++) {
           displayBox.innerText.length,
         ),
       );
+      console.log(firstNumber, typeof firstNumber);
+      console.log(secondNumber, typeof secondNumber);
       displayBox.innerText = operate(operator, firstNumber, secondNumber);
     });
   } else if (btn[i].textContent === "clear") {
