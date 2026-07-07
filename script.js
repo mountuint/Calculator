c083cb3756ee933708ed3f4307c9d792113a6c6e
mountuint <mountuint@protonmail.com>
2026-07-07 12:36:26 +0300

    Add operate function

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function addNumbers(firstNumber, secondNumber) {
-  return console.log(firstNumber + secondNumber);
+  return firstNumber + secondNumber;
 }
 
 function extractNumbers(firstNumber, secondNumber) {
-  return console.log(firstNumber - secondNumber);
+  return firstNumber - secondNumber;
 }
 
 function multiplyNumbers(firstNumber, secondNumber) {
-  return console.log(firstNumber * secondNumber);
+  return firstNumber * secondNumber;
 }
 
 function divideNumbers(firstNumber, secondNumber) {
-  return console.log(firstNumber / secondNumber);
+  return firstNumber / secondNumber;
 }
 
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function operate(operator, firstNumber, secondNumber) {
+  if(operator === "+") {
+    return addNumbers(firstNumber, secondNumber)
+  } else if(operator === "-") {
+    return extractNumbers(firstNumber, secondNumber)
+  } else if(operator === "*") {
+    return multiplyNumbers(firstNumber, secondNumber)
+  } else if(operator === "/") {
+    return divideNumbers(firstNumber, secondNumber)
+  }
+}
