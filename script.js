2d5f151f16fabcad8af6c5e9a5b05f7682d6fdcc
mountuint <mountuint@protonmail.com>
2026-07-07 15:13:02 +0300

    Add eventListeners to the buttons

--- script.js
+++ script.js
@@ -19,13 +19,37 @@ let secondNumber = 0;
 let operator = "";
 
 function operate(operator, firstNumber, secondNumber) {
-  if(operator === "+") {
-    return addNumbers(firstNumber, secondNumber)
-  } else if(operator === "-") {
-    return extractNumbers(firstNumber, secondNumber)
-  } else if(operator === "*") {
-    return multiplyNumbers(firstNumber, secondNumber)
-  } else if(operator === "/") {
-    return divideNumbers(firstNumber, secondNumber)
+  if (operator === "+") {
+    return addNumbers(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return extractNumbers(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiplyNumbers(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divideNumbers(firstNumber, secondNumber);
+  }
+}
+
+const btn = document.querySelectorAll("button");
+const displayBox = document.querySelector(".displayBox");
+
+for (let i = 0; i < btn.length; i++) {
+  if (Number.isFinite(Number(btn[i].textContent))) {
+    btn[i].addEventListener("click", () => {
+      displayBox.textContent += btn[i].textContent;
+    });
+  } else if (
+    btn[i].textContent === "+" ||
+    btn[i].textContent === "-" ||
+    btn[i].textContent === "/" ||
+    btn[i].textContent === "*"
+  ) {
+    btn[i].addEventListener("click", () => {
+      displayBox.textContent += btn[i].textContent;
+    })
+  } else if(btn[i].textContent === "=") {
+    btn[i].addEventListener("click", () => {
+      
+    })
   }
 }
