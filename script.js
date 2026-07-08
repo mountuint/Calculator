bd74347ba2f00baa556a9201aad5dd5c44417406
mountuint <mountuint@protonmail.com>
2026-07-08 21:41:11 +0300

    Evaluate the current operation when a second number exists

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function divideNumbers(firstNumber, secondNumber) {
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+let outcome = 0;
 
 function operate(operator, firstNumber, secondNumber) {
   if (operator === "+") {
@@ -55,18 +56,39 @@ for (let i = 0; i < btn.length; i++) {
       displayBox.textContent += btn[i].textContent;
       operator = btn[i].textContent;
       console.log(operator);
+      console.log(displayBox.textContent)
+
+      if (secondNumber !== "") {
+        outcome = operate(
+          operator,
+          Number(firstNumber),
+          Number(secondNumber),
+        );
+        displayBox.textContent = operate(
+          operator,
+          Number(firstNumber),
+          Number(secondNumber),
+        );
+        alert(outcome)
+      }
     });
-  } else if (btn[i].textContent === "=") {
-    btn[i].addEventListener("click", () => {
+  } else if (btn[i].textContent === "=") {              //look at this part
+    btn[i].addEventListener("click", () => {   
+      outcome = operate(
+          operator,
+          Number(firstNumber),
+          Number(secondNumber),
+        );         
       displayBox.textContent = operate(
         operator,
         Number(firstNumber),
         Number(secondNumber),
       );
+      alert(outcome)
     });
   } else if (btn[i].textContent === ".") {
     btn[i].addEventListener("click", () => {
-      displayBox.textContent += btn[i].textContent
+      displayBox.textContent += btn[i].textContent;
       if (operator !== "") {
         secondNumber += btn[i].textContent;
         console.log("this is the secondNumber: " + secondNumber);
